2d324dd12eda8b45ac8038d854d01c61f36e168b
Khash-Erdene <khasherdeneswe@gmail.com>
2023-12-01 15:45:37 +0800

    main page mock

--- src/app/(MainRoutes)/main/page.tsx
+++ src/app/(MainRoutes)/main/page.tsx
@@ -18,43 +18,51 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { BookOpenText, School } from "lucide-react";
 
 const page = () => {
   return (
-    <Card className="w-[350px]">
-      <CardHeader>
-        <CardTitle>Create project</CardTitle>
-        <CardDescription>Deploy your new project in one-click.</CardDescription>
-      </CardHeader>
-      <CardContent>
-        <form>
-          <div className="grid w-full items-center gap-4">
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="name">Name</Label>
-              <Input id="name" placeholder="Name of your project" />
-            </div>
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="framework">Framework</Label>
-              <Select>
-                <SelectTrigger id="framework">
-                  <SelectValue placeholder="Select" />
-                </SelectTrigger>
-                <SelectContent position="popper">
-                  <SelectItem value="next">Next.js</SelectItem>
-                  <SelectItem value="sveltekit">SvelteKit</SelectItem>
-                  <SelectItem value="astro">Astro</SelectItem>
-                  <SelectItem value="nuxt">Nuxt.js</SelectItem>
-                </SelectContent>
-              </Select>
-            </div>
+    <article className="">
+      <section className="flex flex-col items-center  justify-center pt-5">
+        <p>Tyson AGE 19</p>
+        <p>Ajilgui</p>
+      </section>
+      <section className="flex gap-2 pt-[22vh]">
+        <Card className="w-[350px] h-[260px] py-10">
+          <CardHeader>Их дээд сургуульд сурах уу?</CardHeader>
+          <CardContent>
+            <School />
+          </CardContent>
+        </Card>
+        <Card className="w-[350px] h-[260px] py-10">
+          <CardHeader> Gap year авах уу?</CardHeader>
+          <CardContent>
+            <BookOpenText />
+          </CardContent>
+        </Card>
+      </section>
+      <section className="pt-14">
+        <Card className="flex p-3">
+          <div className="flex flex-col gap-3 py-2">
+            <p className="text-sm">Happiness: 70</p>
+            <p className="text-sm">Intelligence: 70</p>
+            <p className="text-sm">Physique: 70</p>
           </div>
-        </form>
-      </CardContent>
-      <CardFooter className="flex justify-between">
-        <Button variant="outline">Cancel</Button>
-        <Button>Deploy</Button>
-      </CardFooter>
-    </Card>
+          <div className="p-5">
+            <img
+              src="https://www.w3schools.com/w3css/img_avatar3.png"
+              alt=""
+              className="rounded-full  w-20 h-20"
+            />
+          </div>
+          <div className="flex flex-col gap-3 py-2">
+            <p className="text-sm">Health: 70</p>
+            <p className="text-sm">Creativity: 70</p>
+            <p className="text-sm">Wealth: 70</p>
+          </div>
+        </Card>
+      </section>
+    </article>
   );
 };
 
