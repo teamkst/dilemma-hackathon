ef4c543fae8faf79a647089a1582a59f877dbcc3
Khash-Erdene <khasherdeneswe@gmail.com>
2023-12-03 16:41:36 +0800

    last step

--- src/app/(MainRoutes)/profession/page.tsx
+++ src/app/(MainRoutes)/profession/page.tsx
@@ -1,17 +1,31 @@
+"use client";
 import { BottomTab } from "@/components/main/BottomTab";
 import { Job } from "@/components/profession/Job";
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 import Waiter from "@/../../public/waiter.png";
 import Cashier from "@/../../public/cashier1.png";
 import Digitalist from "@/../../public/digitalist.png";
 
 const page = () => {
+  const [select, setSelect] = useState(false);
+  const handleButtonClick = () => {
+    // Toggle the select state on button click
+    setSelect(!select);
+  };
   return (
-    <div className="flex w-full h-screen flex-col bg-blue-400 px-6 pt-6 space-y-3">
+    <div className="flex w-full h-screen flex-col bg-[#14172A] px-6 pt-6 space-y-3">
       <div className="flex justify-between w-full items-center">
         <p className="text-white text-[32px] font-extrabold">Ажил</p>
-        <div>nmg soli</div>
+        <button className="rounded-full relative inline-flex group items-center justify-center px-3.5 py-3.5 m-1 cursor-pointer border-b-4 border-l-2 active:border-[#fff] active:shadow-none shadow-lg bg-gradient-to-tr from-[#fff] to-[#Fff] border-[#f5f5f5] text-black gap-2">
+          <span className="absolute w-0 h-0 transition-all duration-300 ease-out bg-white rounded-full group-hover:w-32 group-hover:h-32 opacity-10"></span>
+          <span className="relative">
+            <Image src={"assets/coin.svg"} width={25} height={18} alt="coin" />
+          </span>
+          <p className=" font-bold text-[20px]">
+            {localStorage.getItem("coin")}
+          </p>
+        </button>
       </div>
       <div className="flex flex-col space-y-3">
         <p className="text-white text-[16px] font-extrabold">Цагийн ажил</p>
@@ -23,12 +37,14 @@ const page = () => {
               <Image alt="job" src={Waiter} width={10} height={10} />
               <p className="text-white text-[20px] font-extrabold">120</p>
             </div>
-            <button>
+            <button onClick={() => setSelect(!select)}>
               <a
                 href="#_"
-                className="inline-flex items-center justify-center w-full px-8 py-2 text-base font-bold leading-6 text-white bg-indigo-600 border border-transparent rounded-full md:w-auto hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-600"
+                className={`inline-flex items-center justify-center w-full px-8 py-2 text-base font-bold leading-6 text-white bg-indigo-600 border border-transparent rounded-full md:w-auto hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-600 ${
+                  select ? "!text-[9px] !bg-purple-500" : ""
+                }`}
               >
-                Ажиллах
+                {select ? "Ажиллаж байгаа" : "Ажиллах"}
               </a>
             </button>
           </div>
