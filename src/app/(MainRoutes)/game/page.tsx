49ae5e555ef156349e80e4437c4c09ed5193eb85
Taivnaa032 <etaivnaa032@gmail.com>
2023-12-03 16:11:18 +0800

    job

--- src/app/(MainRoutes)/game/page.tsx
+++ src/app/(MainRoutes)/game/page.tsx
@@ -6,11 +6,11 @@ import {
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
-import { ArrowLeftIcon } from "lucide-react";
 import Image from "next/image";
 import React, { useState } from "react";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
+import { Goback } from "@/components/game/Goback";
 
 const page = () => {
   return (
@@ -61,14 +61,7 @@ const page = () => {
 
       {/* Page */}
       <div className="flex justify-between w-full ">
-        <div>
-          <button className="rounded-full relative inline-flex group items-center justify-center px-3.5 py-3.5 m-1 cursor-pointer border-b-4 border-l-2 active:border-[#fff] active:shadow-none shadow-lg bg-gradient-to-tr from-[#fff] to-[#Fff] border-[#f5f5f5] text-black">
-            <span className="absolute w-0 h-0 transition-all duration-300 ease-out bg-white rounded-full group-hover:w-32 group-hover:h-32 opacity-10"></span>
-            <span className="relative">
-              <ArrowLeftIcon />
-            </span>
-          </button>
-        </div>
+        <Goback />
         <button className="rounded-full relative inline-flex group items-center justify-center px-3.5 py-3.5 m-1 cursor-pointer border-b-4 border-l-2 active:border-[#fff] active:shadow-none shadow-lg bg-gradient-to-tr from-[#fff] to-[#Fff] border-[#f5f5f5] text-black gap-2">
           <span className="absolute w-0 h-0 transition-all duration-300 ease-out bg-white rounded-full group-hover:w-32 group-hover:h-32 opacity-10"></span>
           <span className="relative">
