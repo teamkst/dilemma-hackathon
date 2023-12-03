7d73cc9b0634ff2fb2f614e23a74e9a222b38d00
Khash-Erdene <khasherdeneswe@gmail.com>
2023-12-03 16:48:16 +0800

    done

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  ignoreBuildErrors: true,
-};
+const nextConfig = {};
 
-module.exports = nextConfig;
+module.exports = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+};
